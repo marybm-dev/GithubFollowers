bc958662599a61f8fc6e637e993b3848f2f09dde
Mary <mlmartinez85@gmail.com>
2022-06-23 19:13:49 -0700

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Mary Baptista Martinez on 6/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
