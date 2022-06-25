ee28c8d2e228a9fb2518b978ed53db37d9d458ec
Mary <mlmartinez85@gmail.com>
2022-06-24 17:56:30 -0700

    set up view hierarcy and reusable views

---
+++ GithubFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GithubFollowers
+//
+//  Created by Mary Baptista Martinez on 6/24/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      view.backgroundColor = .systemBlue
+    }
+}
---
+++ GithubFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVCViewController.swift
+//  GithubFollowers
+//
+//  Created by Mary Baptista Martinez on 6/24/22.
+//
+
+import UIKit
+
+class SearchViewVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      view.backgroundColor = .systemPink
+    }
+
+}
