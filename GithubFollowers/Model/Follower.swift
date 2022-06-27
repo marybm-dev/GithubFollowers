4605ce3ac270df1273695177d147f122d6cc3cd5
Mary <mlmartinez85@gmail.com>
2022-06-27 16:46:04 -0700

    add user and follower models

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Mary Baptista Martinez on 6/27/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+  var login: String
+  var avatarUrl: String
+}
