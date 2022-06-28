ad52e4f382af4bb46e3f9541e848a3964c0d5ebb
Mary <mlmartinez85@gmail.com>
2022-06-27 17:50:04 -0700

    get followers

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -14,6 +14,15 @@ class FollowerListVC: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
+    NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+      guard let followers = followers else {
+        self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
+        return
+      }
+
+      print("Followers.count = \(followers.count)")
+      print(followers)
+    }
   }
 
   override func viewWillAppear(_ animated: Bool) {
