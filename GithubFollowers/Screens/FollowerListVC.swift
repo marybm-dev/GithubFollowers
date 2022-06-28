bf0264d92694880e3aeb4e62c634805f5fb0d30d
Mary <mlmartinez85@gmail.com>
2022-06-27 18:48:07 -0700

    set up flow layout

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -30,10 +30,25 @@ class FollowerListVC: UIViewController {
   }
 
   func configureCollectionView() {
-    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
     view.addSubview(collectionView)
     collectionView.backgroundColor = .systemPink
     collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
+  }
+
+  func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+    let width = view.bounds.width
+    let padding: CGFloat = 12
+    let minimumItemSpacing: CGFloat = 10
+    let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+    let itemWidth = availableWidth / 3
+
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+    flowLayout.minimumInteritemSpacing = minimumItemSpacing
+    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+
+    return UICollectionViewFlowLayout()
   }
 
   func getFollowers() {
