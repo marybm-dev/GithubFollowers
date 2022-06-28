c6ae3637fcd33d4767320b7569755eae15de80d7
Mary <mlmartinez85@gmail.com>
2022-06-27 18:03:19 -0700

    refactor to use result type

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -14,14 +14,15 @@ class FollowerListVC: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
-    NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
-      guard let followers = followers else {
-        self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
-        return
-      }
 
-      print("Followers.count = \(followers.count)")
-      print(followers)
+    NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+      switch result {
+      case .success(let followers):
+        print(followers)
+
+      case .failure(let error):
+        self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
+      }
     }
   }
 
