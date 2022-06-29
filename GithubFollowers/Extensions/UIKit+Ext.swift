fa1aed4c88d5ec7612ed0cb8c7e955f56f6ff5c4
Mary <mlmartinez85@gmail.com>
2022-06-28 18:42:40 -0700

    house keeping

---
+++ GithubFollowers/Extensions/UIKit+Ext.swift
@@ -0,0 +1,39 @@
+//
+//  UIKit+Ext.swift
+//  GithubFollowers
+//
+//  Created by Mary Baptista Martinez on 6/26/22.
+//
+
+import UIKit
+
+extension UIViewController {
+
+  func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+    DispatchQueue.main.async {
+      let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+      alertVC.modalPresentationStyle = .overFullScreen
+      alertVC.modalTransitionStyle = .crossDissolve
+      self.present(alertVC, animated: true)
+    }
+  }
+}
+
+extension UICollectionViewFlowLayout {
+
+  static func threeColumns(in view: UIView) -> UICollectionViewFlowLayout {
+    let width = view.bounds.width
+    let padding: CGFloat = 12
+    let minimumItemSpacing: CGFloat = 10
+    let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+    let itemWidth = availableWidth / 3
+
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+    flowLayout.minimumInteritemSpacing = minimumItemSpacing
+    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+
+    return flowLayout
+  }
+
+}
--- GithubFollowers/Utilities/UIHelper.swift
+++ GithubFollowers/Utilities/UIHelper.swift
@@ -12,22 +12,3 @@ struct UIHelper {
   static var shouldAnimate: Bool { !UIAccessibility.isReduceMotionEnabled }
 
 }
-
-extension UICollectionViewFlowLayout {
-
-  static func threeColumns(in view: UIView) -> UICollectionViewFlowLayout {
-    let width = view.bounds.width
-    let padding: CGFloat = 12
-    let minimumItemSpacing: CGFloat = 10
-    let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-    let itemWidth = availableWidth / 3
-
-    let flowLayout = UICollectionViewFlowLayout()
-    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-    flowLayout.minimumInteritemSpacing = minimumItemSpacing
-    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-
-    return flowLayout
-  }
-
-}
