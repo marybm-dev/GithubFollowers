50dc7031e4d42bf4fe9162e3f6a5dacfed2b6891
Mary <mlmartinez85@gmail.com>
2022-06-30 16:27:58 -0700

    set up user info

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -18,6 +18,7 @@ class FollowerListVC: UIViewController {
   var filteredFollowers = [Follower]()
   var page = 1
   var hasMoreFollowers = true
+  var isSearching = false
 
   var collectionView: UICollectionView!
   var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -115,18 +116,28 @@ extension FollowerListVC: UICollectionViewDelegate {
     }
   }
 
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let activeArray = isSearching ? filteredFollowers : followers
+    let follower = activeArray[indexPath.item]
+
+    let destinationVC = UserInfoVC()
+    destinationVC.username = follower.login
+    let navController = UINavigationController(rootViewController: destinationVC)
+    present(navController, animated: UIHelper.shouldAnimate)
+  }
 }
 
 extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
 
   func updateSearchResults(for searchController: UISearchController) {
     guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
-
+    isSearching = true
     filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
     updateData(on: filteredFollowers)
   }
 
   func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    isSearching = false
     updateData(on: followers)
   }
 }
