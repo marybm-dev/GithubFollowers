54039e53d6b92673da439bb99d6fd7d10ace6767
Mary <mlmartinez85@gmail.com>
2022-06-29 17:48:42 -0700

    display loading state

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -48,8 +48,10 @@ class FollowerListVC: UIViewController {
   }
 
   func getFollowers() {
+    showLoadingView()
     NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
       guard let self = self else { return }
+      self.dismissLoadingView()
 
       switch result {
       case .success(let followers):
